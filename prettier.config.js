da3daf322746b95a256500a943d12a2ea7346596
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2025-05-18 11:41:21 +0100

    Initial commit of the Next.js project

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: false,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
